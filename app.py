21cb1821f55599dae6a111fc8fcec7b886783bec
fhzm1234/rootkit <95896906+fhzm1234@users.noreply.github.com>
2022-05-17 00:02:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from scapy.all import *
 from flask import *
 from time import *
-import os, threading, datetime
-
+import os, threading, datetime, re
 
 attack = False
 lim = 2000
 i = 0
-ip = 0
-port = 0
+ip_s = 0
+ip_d = 0
+port_s = 0
+port_d = 0
 risk = 0
 i_syn = 0
 i_ack = 0
-i_result = 0
+i_syn_dos = 0
+ip_list=[]
 """
 scpay로 초당 트래픽당 syn, ack확인을 통한 DDOS탐지 방법
 """
 def packet_print(packet):
-    global i, ip, port, i_syn, i_ack
+    global i, i_syn, i_ack, ip_s, ip_d, port_s, port_d
     SYN = 0x02
     ACK = 0x10
 
@@ -35,40 +37,40 @@ def packet_print(packet):
     elif TCP.flags & ACK:
         i_ack += 2
 
-    ip = IP.src
-    port = IP.sport
+    ip_s = IP.src
+    port_s = IP.sport
+
+    ip_d = IP.dst
+    port_d = IP.dport
 
 def start():
     while 1:
-        global i, ip, port, i_syn, i_ack, i_result
+        global i, i_syn, i_ack, ip_s, ip_d, port_s, port_d, i_syn_dos
         i = 0
-        ip = 0
-        port = 0
+        ip_s = 0
+        port_s = 0
+        ip_d = 0
+        port_d = 0
         i_syn = 0
         i_ack = 0
-        i_result = 0
+        i_syn_dos = 0
 
         def ddos_find():
-            global attack, risk, i_syn, i_ack, i_result
+            global attack, risk, i_syn, i_ack, i_syn_dos
             #전체 패킷에 대한 syn의 비율
             try:
-                i_result = i_syn / i
+                i_syn_dos = i_syn / i
             except:
                 risk = 3
 
-            #테스트해보면 DDOS의 경우 result값이 매우 높은 수치를 가짐
-            
-            if i>lim:
+            if i>lim or (i_syn_dos > 0.9 and i_syn > 400) or (i_ack > 400):
                 if attack:
-                    if i_result > 0.9 and i_syn > 400:
                         risk = 0
                 else:
-                    if i_result > 0.9 and i_syn > 400:
                         risk = 1
                         attack = True
             else:
                 if attack:
-                    if i_result > 0.9:
                         risk = 2
                 risk = 3
                 attack = False
@@ -79,30 +81,34 @@ def start():
 t = threading.Thread(target=start)
 t.start()
 
+def ip_list(ip1):
+    if re.match("[A]+", ip1):
+        print("A")
+    if re.match("[D]+", ip1):
+        print("D")
+
+
 def ddos_log():
     while True:
         if risk == 0:
-            ddos_log = datetime.datetime.now().strftime('%H:%M:%S')+" "+"More than once Detect DDOS Attack!"+" Attacker IP: "+ str(ip) + "  Attacker Port: " + str(port) + " Traffic : " + str(i)+" " +str(i_syn)+" "+str(i_ack)+" "+str(i_result) + "\n"
+            ddos_log = datetime.datetime.now().strftime('%H:%M:%S')+" "+"DDOS Detect!" + " Src IP : "+ str(ip_s) +" ==> "+ "Dst IP : "+str(ip_d) +"   Traffic : " +str(i)+"\n"
         elif risk == 1:
-            ddos_log = datetime.datetime.now().strftime('%H:%M:%S')+" "+"First Detect DDOS Attack!"+" Attacker IP: "+ str(ip) + "  Attacker Port: " + str(port) + " Traffic : " + str(i)+" " +str(i_syn)+" "+str(i_ack)+" "+str(i_result) + "\n"
+            ddos_log = datetime.datetime.now().strftime('%H:%M:%S')+" "+"First DDOS Attack!"+" Src IP : "+ str(ip_s) +" ==> "+ "Dst IP : "+str(ip_d) +"   Traffic : " +str(i)+"\n"
         elif risk == 2:
-            ddos_log = datetime.datetime.now().strftime('%H:%M:%S')+" "+"Detect DDOS Attack!"+" Attacker IP: "+ str(ip) + "  Attacker Port: " + str(port) + " Traffic : " + str(i)+" " +str(i_syn)+" "+str(i_ack)+" "+str(i_result) + "\n"
+            ddos_log = datetime.datetime.now().strftime('%H:%M:%S')+" "+"Suspect DDOS" +" Src IP : "+ str(ip_s) +" ==> "+ "Dst IP : "+str(ip_d) +"   Traffic : " +str(i)+"\n"
         else:
-            ddos_log = datetime.datetime.now().strftime('%H:%M:%S')+" "+"Not Found DDOS Attack" +" Traffic : " + str(i)+" " +str(i_syn)+" "+str(i_ack)+" "+str(i_result)+ "\n"
+            ddos_log = datetime.datetime.now().strftime('%H:%M:%S')+" "+"Not Found DDOS Attack" +" Src IP : "+ str(ip_s) +" ==> "+ "Dst IP : "+str(ip_d) +"   Traffic : " +str(i)+"\n"
 
         yield ddos_log.encode()
         sleep(1)
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
-def root():
+def index():
+    if request.method == "POST":
+        ip_black = request.form["ip_black"]
+        ip_list(ip_black)
     return render_template("index.html")
-
-@app.route('/param', methods=['GET', 'POST'])
-def param():
-    ip = request.args.get('ip')
-    print (ip)
-    return f'ip : {ip}'
 
 @app.route('/log', methods=["GET", "POST"])
 def stream():
